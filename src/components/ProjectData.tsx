db7a6b30a30ad7ea415b65454e5dacc91c911684
vinitsharma101 <v7056s@gmail.com>
2025-08-26 15:49:37 +0530

    update projects

--- src/components/ProjectData.tsx
+++ src/components/ProjectData.tsx
@@ -20,20 +20,12 @@ export const projects: Project[] = [
     technologies: ["React", "Node.js", "Express", "MongoDB", "Redux"],
     images: [
       {
-        url: "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-        alt: "Music streaming app interface",
-      },
-      {
         url: "https://images.unsplash.com/photo-1511379938547-c1f69419868d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
         alt: "Music player view",
       },
-      {
-        url: "https://images.unsplash.com/photo-1494232410401-ad00d5433cfa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-        alt: "Playlist management",
-      },
     ],
     githubUrl: "https://github.com/Vinitsharma101/MusicPLayer...-MERN-",
-    liveUrl: "https://soulifyy.vercel.app/",
+    liveUrl: "https://xonicbeatz.vercel.app/",
   },
   {
     title: "Cryptocurrency Tracker",
@@ -45,19 +37,11 @@ export const projects: Project[] = [
         url: "https://images.unsplash.com/photo-1621761191319-c6fb62004040?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80",
         alt: "Crypto dashboard",
       },
-      {
-        url: "https://images.unsplash.com/photo-1590283603385-17ffb3a7f29f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-        alt: "Price charts",
-      },
-      {
-        url: "https://images.unsplash.com/photo-1518546305927-5a555bb7020d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80",
-        alt: "Portfolio view",
-      },
     ],
     githubUrl: "https://github.com/Vinitsharma101/KoinXbackend-assignment",
     liveUrl: "https://github.com/Vinitsharma101/KoinXbackend-assignment",
   },
-  
+
   {
     title: "Language Translator App",
     description:
@@ -67,10 +51,6 @@ export const projects: Project[] = [
       {
         url: "https://images.unsplash.com/photo-1523726491678-bf852e717f6a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
         alt: "Language selection",
-      },
-      {
-        url: "https://images.unsplash.com/photo-1492538368677-f6e0afe31dcc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-        alt: "Voice recognition feature",
       },
     ],
     githubUrl: "https://github.com/Vinitsharma101",
@@ -86,13 +66,9 @@ export const projects: Project[] = [
         url: "https://images.unsplash.com/photo-1563986768609-322da13575f3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
         alt: "Chat interface with user messages",
       },
-      {
-        url: "https://imgs.search.brave.com/P9JidKoNfl3ue8pp9Z4lR454EcUl9W4tbXpFCTCHmjc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/ZnJlZWNvZGVjYW1w/Lm9yZy9uZXdzL2Nv/bnRlbnQvaW1hZ2Vz/LzIwMjMvMDEvaW1h/Z2U5LTIucG5n",
-        alt: "Contact list with online users",
-      },
     ],
     githubUrl: "https://github.com/Vinitsharma101/Chat-app",
-    liveUrl: "https://github.com/Vinitsharma101/Chat-app",
+    liveUrl: "https://chattriix.vercel.app/",
   },
   {
     title: "Location Sharing Web App",
@@ -108,7 +84,7 @@ export const projects: Project[] = [
     ],
     images: [
       {
-        url: "public/assets/locateme.png",
+        url: "assets/locateme.png",
         alt: "Map interface showing real-time friends' locations",
       },
     ],
@@ -129,7 +105,7 @@ export const projects: Project[] = [
     ],
     images: [
       {
-        url: "public/assets/promanager.png", 
+        url: "assets/promanager.png",
         alt: "Dashboard view of project management platform with tasks and projects overview",
       },
     ],
